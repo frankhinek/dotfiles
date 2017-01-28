57ff636bf93c606bb52178a401d51dbbf9ea41c2
Frank Hinek <frankhinek@users.noreply.github.com>
2017-01-28 07:11:08 -0500

    Updating antibody on OSX on dot_update
    Added featured to dotfiles topic for antibody to update to the latest version when dot_update is executed.

--- topics/antibody/install.sh
+++ topics/antibody/install.sh
@@ -1,4 +1,10 @@
 #!/bin/zsh
+if which brew >/dev/null 2>&1; then
+  brew untap -q getantibody/homebrew-antibody || true
+  brew tap -q getantibody/homebrew-antibody
+  brew install antibody
+fi
+
 antibody bundle < "$DOTFILES_ROOT/topics/antibody/bundles.txt" > ~/.bundles.txt
 antibody bundle sindresorhus/pure >> ~/.bundles.txt
 antibody bundle < "$DOTFILES_ROOT/topics/antibody/last_bundles.txt" >> ~/.bundles.txt
