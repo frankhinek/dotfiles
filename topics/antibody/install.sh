3bb8fe40457f81f48924ba7acdc11c2c4dc4025c
Frank Hinek <frankhinek@users.noreply.github.com>
2017-01-28 07:08:06 -0500

    Remove --static from antibody install
    The --static behavior is now the default as of antibody v2.0.0.  The command line parameter was removed.

--- topics/antibody/install.sh
+++ topics/antibody/install.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-antibody bundle --static < "$DOTFILES_ROOT/topics/antibody/bundles.txt" > ~/.bundles.txt
-antibody bundle --static sindresorhus/pure >> ~/.bundles.txt
-antibody bundle --static < "$DOTFILES_ROOT/topics/antibody/last_bundles.txt" >> ~/.bundles.txt
+antibody bundle < "$DOTFILES_ROOT/topics/antibody/bundles.txt" > ~/.bundles.txt
+antibody bundle sindresorhus/pure >> ~/.bundles.txt
+antibody bundle < "$DOTFILES_ROOT/topics/antibody/last_bundles.txt" >> ~/.bundles.txt
