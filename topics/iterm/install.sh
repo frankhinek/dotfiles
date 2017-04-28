89d4eb59e6c1385b69aedb8c7ec451d4ee0cc05d
Frank Hinek <frank.hinek@gmail.com>
2017-04-27 21:39:55 -0400

    Change home directory path to /Users/frank

--- topics/iterm/install.sh
+++ topics/iterm/install.sh
@@ -4,10 +4,10 @@
 [ "$(uname -s)" != "Darwin" ] && exit 0
 
 # The com.googlecode.iterm2.plist.example included in the git repository is
-# configured to use /Users/fhinek as the path prefix for the .dotfiles directory
-# location.  The sed replaces "/Users/fhinek" with  the current user's $HOME
+# configured to use /Users/frank as the path prefix for the .dotfiles directory
+# location.  The sed replaces "/Users/frank" with  the current user's $HOME
 # directory before writing the result to com.googlecode.iterm2.plist.
-sed "s;/Users/fhinek;$HOME;g" \
+sed "s;/Users/frank;$HOME;g" \
   "$DOTFILES_ROOT"/topics/iterm/com.googlecode.iterm2.plist.example > "$DOTFILES_ROOT"/topics/iterm/com.googlecode.iterm2.plist
 
 # Configure iTerm2 to read Preferences from the iterm dotfiles topic
