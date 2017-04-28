ab628550dce073977d4b59ce15257ff71f336bb8
Frank Hinek <frank.hinek@gmail.com>
2017-04-28 10:21:08 -0400

    Fix antibody conditional install or upgrade script

--- topics/antibody/install.sh
+++ topics/antibody/install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 if which brew >/dev/null 2>&1; then
-  brew install getantibody/tap/antibody || brew upgrade antibody
+  if ! which antibody >/dev/null 2>&1; then
+    brew install getantibody/tap/antibody
+  else
+    brew upgrade antibody
+  fi
 fi
 
 antibody bundle < "$DOTFILES/topics/antibody/bundles.txt" > ~/.zsh_plugins.sh
