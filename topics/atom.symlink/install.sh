a4458dd713b40133f415d36188458bacd901aa88
Frank Hinek <frank.hinek@gmail.com>
2017-04-27 22:14:12 -0400

    Modify Atom package installer to only install or remove when necessary

--- topics/atom.symlink/install.sh
+++ topics/atom.symlink/install.sh
@@ -2,13 +2,43 @@
 if test "$(which apm)"; then
   apm upgrade --confirm false
 
-  apm install --packages-file ~/.dotfiles/topics/atom.symlink/packages.txt || true
+  apm_packages="
+    Sublime-Style-Column-Selection
+    advanced-open-file
+    asciidoc-image-helper
+    asciidoc-preview
+    atom-beautify
+    atom-material-syntax
+    atom-material-ui
+    atom-smart-template
+    atom-ternjs
+    autocomplete-asciidoc
+    autocomplete-modules
+    autocomplete-paths
+    busy-signal
+    editorconfig
+    file-icons
+    git-plus
+    intentions
+    language-asciidoc
+    language-babel
+    language-terraform
+    linter
+    linter-eslint
+    linter-terraform-syntax
+    linter-ui-default
+    pigments
+  "
+
+  for package in $apm_packages; do
+    apm list | grep -q "$module" || apm install "$module"
+  done
 
   apm_packages="
     metrics
     exception-reporting
   "
   for package in $apm_packages; do
-    apm remove "$package" || true
+    apm list | grep -q "$module" || apm remove "$module"
   done
 fi
