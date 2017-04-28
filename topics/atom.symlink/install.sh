2f20c18b65c2f6ee16699d6756c3ae48de8e9d52
Frank Hinek <frank.hinek@gmail.com>
2017-04-27 22:24:21 -0400

    Fix variable typo in Atom installer script

--- topics/atom.symlink/install.sh
+++ topics/atom.symlink/install.sh
@@ -31,7 +31,7 @@ if test "$(which apm)"; then
   "
 
   for package in $apm_packages; do
-    apm list | grep "$module" > /dev/null 2>&1 || apm install "$module"
+    apm list | grep "$package" > /dev/null 2>&1 || apm install "$package""
   done
 
   apm_packages="
@@ -39,6 +39,6 @@ if test "$(which apm)"; then
     exception-reporting
   "
   for package in $apm_packages; do
-    apm list | grep "$module" > /dev/null 2>&1 || apm remove "$module"
+    apm list | grep "$package" > /dev/null 2>&1 || apm remove "$package"
   done
 fi
