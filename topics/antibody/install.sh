5f2892a0764fdafc6b5d0f4a2729d93a822d5f59
Frank Hinek <frank.hinek@gmail.com>
2017-04-28 10:14:09 -0400

    Update to antibody 2.3+ and improve installation

--- topics/antibody/install.sh
+++ topics/antibody/install.sh
@@ -1,10 +1,7 @@
-#!/bin/zsh
-if ! which brew >/dev/null 2>&1; then
-  brew untap -q getantibody/homebrew-antibody || true
-  brew tap -q getantibody/homebrew-antibody
-  brew install getantibody/tap/antibody
+#!/bin/sh
+if which brew >/dev/null 2>&1; then
+  brew install getantibody/tap/antibody || brew upgrade antibody
 fi
 
-antibody bundle < "$DOTFILES_ROOT/topics/antibody/bundles.txt" > ~/.bundles.txt
-antibody bundle sindresorhus/pure >> ~/.bundles.txt
-antibody bundle < "$DOTFILES_ROOT/topics/antibody/last_bundles.txt" >> ~/.bundles.txt
+antibody bundle < "$DOTFILES/antibody/bundles.txt" > ~/.zsh_plugins.sh
+antibody update
