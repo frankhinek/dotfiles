d114e3255037b3db51f1cbf5308c4deed0058370
Frank Hinek <frank.hinek@gmail.com>
2018-01-30 14:03:46 -0500

    Update PyCharm non-project file color

--- topics/pycharm.symlink/install.sh
+++ topics/pycharm.symlink/install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 if [ "$(uname -s)" = "Darwin" ]; then
-	# Set a user environment variable that tells PyCharm where to find the idea.properties file
-	ln -sf "$DOTFILES_ROOT/topics/pycharm.symlink/pycharm.properties.plist" "$HOME/Library/LaunchAgents/pycharm.properties.plist"
+    # Set a user environment variable that tells PyCharm where to find the idea.properties file
+    ln -sf "$DOTFILES_ROOT/topics/pycharm.symlink/pycharm.properties.plist" "$HOME/Library/LaunchAgents/pycharm.properties.plist"
+
+    # Install PyCharm files that change regularly during use due to last update counters
+    cat "$DOTFILES_ROOT"/topics/pycharm.symlink/config/colors/_@user_Darcula.icls.example > "$DOTFILES_ROOT"/topics/pycharm.symlink/config/colors/_@user_Darcula.icls
+    cat "$DOTFILES_ROOT"/topics/pycharm.symlink/config/options/jdk.table.xml.example > "$DOTFILES_ROOT"/topics/pycharm.symlink/config/options/jdk.table.xml
+    cat "$DOTFILES_ROOT"/topics/pycharm.symlink/config/options/options.xml.example > "$DOTFILES_ROOT"/topics/pycharm.symlink/config/options/options.xml
 fi
